2966381c27b71978d16583a587ad398ccf82fc4b
Brian Iverson <brian.iverson@tuebora.com>
2023-04-20 13:10:46 -0500

    Add auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,36 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { ClerkProvider, RedirectToSignIn, SignedIn, SignedOut } from '@clerk/nextjs';
+import { useRouter } from 'next/router';
+
+const publicPages = ["/sign-in/[[...index]]"];
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  // Get the pathname
+  const { pathname } = useRouter();
+
+  // Check if the current route matches a public page
+  const isPublicPage = publicPages.includes(pathname);
+  
+  // If the current route is lised as public, render it directly
+  // Otherwise, use Clerk to require authentication
+  return (
+    <ClerkProvider {...pageProps} >
+      {isPublicPage ? (
+        <Component {...pageProps} />
+      ) : (
+        <>
+          <SignedIn>
+            <Component {...pageProps} />
+          </SignedIn>
+          <SignedOut>
+            <RedirectToSignIn />
+          </SignedOut>
+        </>
+      )}
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
